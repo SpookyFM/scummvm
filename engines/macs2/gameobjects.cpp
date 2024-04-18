31a39acf6c668451ac9d6bda96ab25a62bf286b2
Florian Mehm <florian@mehm.net>
2024-04-19 01:33:55 +0200

    Working on first scene implementation

--- engines/macs2/gameobjects.cpp
+++ engines/macs2/gameobjects.cpp
@@ -1,8 +1,32 @@
 #include "gameobjects.h"
+#include "common/memstream.h"
 
 namespace Common {
 
 DECLARE_SINGLETON(Macs2::GameObjects);
 DECLARE_SINGLETON(Macs2::Scenes);
 
+} // namespace Common
+
+Common::MemoryReadStream *Macs2::Scenes::ReadSceneScript(uint16_t sceneIndex, Common::MemoryReadStream *fileStream) {
+	// Calculate the offset of the script data offset
+	// This addressing can be found in the l0037_2856 code block
+
+	uint16_t sceneDataOffset = sceneIndex * 0xC;
+	// Offset of the data in [0752h] global
+	constexpr uint16_t globalDataOffset = 0xC + 0x4;
+	sceneDataOffset += globalDataOffset;
+	fileStream->seek(sceneDataOffset - 0x8);
+	uint32_t sceneDataOffset2 = fileStream->readUint32LE();
+	fileStream->seek(sceneDataOffset2, SEEK_SET);
+	
+
+	// Read the script from there
+	// We read 80h bytes
+	fileStream->seek(0x80, SEEK_CUR);
+	uint16_t scriptSize = fileStream->readUint16LE();
+	uint8_t *scriptData = new uint8_t[scriptSize];
+	fileStream->read(scriptData, scriptSize);
+	// TODO: Consider using the endian version for all the memoryReadStreams
+	return new Common::MemoryReadStream(scriptData, scriptSize);
 }
