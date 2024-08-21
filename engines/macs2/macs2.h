2a95ff75a36ddb71fc840ba670ca363296c9d312
Florian Mehm <florian@mehm.net>
2024-08-21 02:19:34 +0200

    Work on 1480

--- engines/macs2/macs2.h
+++ engines/macs2/macs2.h
@@ -84,7 +84,7 @@ struct BackgroundAnimationBlob {
 	Common::Array<uint8_t> Blob;
 	uint32 FrameIndex;
 	AnimFrame GetFrame(uint32_t index);
-	bool GetIsAutoUpdated(uint16_t bpp8) const;
+	bool GetIsAutoUpdated(bool bpp6, uint16_t bpp8) const;
 
 	// Placing reference implementation here for now
 	static void Func1480(bool writeResult, Common::MemoryReadStream *stream);
