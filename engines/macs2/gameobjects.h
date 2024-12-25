bf768e0f3f9fc1dda2ad30a0603933122b14a1bc
Florian Mehm <florian@mehm.net>
2024-12-25 12:43:36 +0100

    Updates

--- engines/macs2/gameobjects.h
+++ engines/macs2/gameobjects.h
@@ -74,7 +74,8 @@ public:
 	// Index of the object, starting at 1
 	uint16_t Index;
 
-	int testOverloadAnimation = -1;
+	Common::Array<uint8_t> overloadAnimation;
+	bool useOverloadAnimation = false;
 
 	// These are the values read by the code around l0037_082D:
 	Common::Point Position;
