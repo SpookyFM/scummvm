211306c8f1da0f95cbe24879646252fd562b95e1
Florian Mehm <florian@mehm.net>
2025-02-01 00:44:21 +0100

    Updates

--- engines/macs2/adlib.h
+++ engines/macs2/adlib.h
@@ -181,6 +181,9 @@ namespace Macs2 {
 
 		// Reads a byte from the specified offset without changing the position in the stream
 		uint8_t peekByteAt(uint16_t offset);
+
+		// fn0017_2A80: 0017:2A80
+		uint8_t FuncA280();
 	
 
 	public:
